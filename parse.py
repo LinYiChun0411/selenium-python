a3d5337e10062e35087aad048bd95baa6cc0fa19
LinYiChun0411 <alanlin0411@gmail.com>
2020-03-11 10:02:13 +0800

    update print format

--- parse.py
+++ parse.py
@@ -14,15 +14,17 @@ def wait_for_ajax(driver):
 driver = webdriver.Chrome()
 
 driver.get("http://spys.one/free-proxy-list/CN/")
+
 wait_for_ajax(driver)
 
 table = driver.find_element_by_xpath("/html/body/table[2]/tbody/tr[5]/td/table")
 
 trList = table.find_elements_by_tag_name("tr")
 
-title = trList.__getitem__(2)
-print(title.text)
+for i in range(2, len(trList)):
+    tdList = trList[i].find_elements_by_css_selector("td[colspan='1']")
+    if tdList:
+        print(tdList[0].text + "," + tdList[1].text + "," + tdList[2].text + "," + tdList[3].text + "," + tdList[
+            4].text + "," + tdList[5].text + "," + tdList[6].text + "," + tdList[7].text + "," + tdList[8].text)
 
-for i in range(3, trList.__len__()):
-    print(trList.__getitem__(i).text)
 driver.close()
